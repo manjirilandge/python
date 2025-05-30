38f6b569b16eddfeab92b32ea2fa17eaf4cdaec4
manjirilandge <landgemanjiri@gmail.com>
2025-05-30 15:35:09 +0530

    done

---
+++ complex_num.py
@@ -0,0 +1,7 @@
+# Write a program for complex number
+
+a=5+5j
+print(type(a))
+print(a.conjugate)
+print(a.imag)
+print(a.real)
---
+++ input.py
@@ -0,0 +1,8 @@
+#Write a program to take input
+
+#a=input(int("Enter first numbers:-"),a)
+#b=input(int("Enter second number:-"),b)
+#a=int(input("Enter a number"))
+#b=int(input("Enter a number"))
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
